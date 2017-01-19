72996e4ad4ec966021301611e184c2aac3eb3d32
Michał Urbaniak <michalu@man.poznan.pl>
2017-01-19 12:47:15 +0100

    refactor-sample-app: new init method

--- sample-app/src/main/java/pl/psnc/indigo/omt/sampleapp/IndigoApplication.java
+++ sample-app/src/main/java/pl/psnc/indigo/omt/sampleapp/IndigoApplication.java
@@ -1,6 +1,7 @@
 package pl.psnc.indigo.omt.sampleapp;
 
 import android.app.Application;
+import java.net.URISyntaxException;
 import pl.psnc.indigo.omt.Indigo;
 
 /**
@@ -10,6 +11,10 @@ public class IndigoApplication extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
-        Indigo.init(this, "futuregateway");
+        try {
+            Indigo.init(this, "futuregateway");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Bad server address. It should be http(s)://ip:port");
+        }
     }
 }
